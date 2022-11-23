0f6e179002ce2430e3140a6054711b2da3344949
Rakunyo <i.plachecka98@gmail.com>
2022-11-23 01:30:46 +0100

    Moved Play/Pause buttoto it's own file and added icons

--- src/components/Player/Player.component.tsx
+++ src/components/Player/Player.component.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import usePlayer from "~/hooks/usePlayer.hook";
-import trackData from "~/data/trackData.json"
+import StateButton from './Components/StateButton/StateButton.component';
 
 import styles from "./Player.module.css";
 
 const Player = () => {
-  const { state, actions} = usePlayer()
   return (
     <div className={styles.root}>
-      {state.playing ? (
-        <button onClick={() => actions.pause()}>pause</button>
-      ) : (
-        <button onClick={() => actions.play({
-          id: trackData.id,
-          name: trackData.name,
-          src: trackData.preview_url,
-          artists: trackData.artists.map(artist => artist.name)
-        })}>play</button>
-      )}
+      <StateButton />
     </div>
   );
 };
